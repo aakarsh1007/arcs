5c32d6a8537698755ae7642687d0040537fd303a
Dominik Winecki <dominikwinecki@gmail.com>
2016-08-01 21:12:57 -0400

    Add comms

---
+++ src/dash/comms.c
@@ -0,0 +1,15 @@
+#include "common.h"
+#include "arcs_net.h"
+#include "comms.h"
+
+void connect() {
+
+}
+
+void desconnect() {
+
+}
+
+void update(struct js_state js_state) {
+
+}
---
+++ src/dash/comms.h
@@ -0,0 +1,10 @@
+#ifndef COMMS_H
+#define COMMS_H
+
+void connect();
+
+void disconnect();
+
+void update(struct js_state);
+
+#endif
