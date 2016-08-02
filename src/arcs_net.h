fe5f6b8dc8dcbc647c8c33aec2f4bbe4acc4564f
Dominik Winecki <dominikwinecki@gmail.com>
2016-08-01 20:34:32 -0400

    Add shared network header

---
+++ src/arcs_net.h
@@ -0,0 +1,7 @@
+/*
+ * This is a file that is refferenced by both dash and remote by a relative symbolic link.
+ * All shared networking structures and constants should be defined here.
+ */
+
+ #define PORT 30000
+
