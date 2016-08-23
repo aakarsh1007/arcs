07dc884abb530a4c4eed41a77371b2f923ffd84f
Dominik Winecki <dominikwinecki@gmail.com>
2016-08-23 16:40:01 -0400

    Add disabled command

---
+++ src/remote/disabled.c
@@ -0,0 +1,11 @@
+#include "common.h"
+#include "drivetrain.h"
+#include "comms.h"
+
+void disabled_init() {
+	drive_update(0, 0);
+}
+
+void disabled_update() {
+	drive_update(0, 0);
+}
---
+++ src/remote/disabled.h
@@ -0,0 +1,8 @@
+#ifndef DISABLED_H
+#define DISABLED_H
+
+void disabled_init();
+
+void disabled_update();
+
+#endif
