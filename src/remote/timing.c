3abc1edbbd6b292a09041bd98de1fd7defce6569
Dominik Winecki <dominikwinecki@gmail.com>
2016-11-12 14:02:11 -0500

    Fix timing issue

--- src/remote/timing.c
+++ src/remote/timing.c
@@ -1,9 +1,12 @@
+#include "common.h"
 #include <sys/time.h>
 #include <stdint.h>
 #include "timing.h"
 
 int64_t microtime() {
 	struct timeval tv;
-	gettimeofday(&tv, 0);
-	return tv.tv_usec;
+	if (gettimeofday(&tv, 0)) {
+		slog(300, SLOG_WARN, "Failed gettimeofday");
+	}
+	return (int64_t)tv.tv_usec + (int64_t)1000000 * (int64_t)tv.tv_sec;
 }
